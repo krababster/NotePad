8fbe572c2d9fa2d04acb87b61c6817e55dc78a9e
krababster <knaidaaa@gmail.com>
2022-04-04 21:01:17 +0300

    added tab file

--- NotePad/Form1.Designer.cs
+++ NotePad/Form1.Designer.cs
@@ -28,13 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьПапкуДокументаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьСтандартнымПриожениемToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьПапкуКакПроектToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обновитьСДискаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКопиюКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.переименоватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.управлениеВкладкамиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьФайлвКорзинуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьСессиюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьСессиюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.печатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.распечататьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьПоследнийЗакрытыйФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьВсеНедавниеФайлыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новыйToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.открытьПапкуДокументаToolStripMenuItem,
+            this.открытьСтандартнымПриожениемToolStripMenuItem,
+            this.выбратьПапкуКакПроектToolStripMenuItem,
+            this.обновитьСДискаToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem,
+            this.сохранитьКопиюКакToolStripMenuItem,
+            this.сохранитьВсеToolStripMenuItem,
+            this.переименоватьToolStripMenuItem,
+            this.закрытьToolStripMenuItem,
+            this.закрытьВсеToolStripMenuItem,
+            this.управлениеВкладкамиToolStripMenuItem,
+            this.удалитьФайлвКорзинуToolStripMenuItem,
+            this.загрузитьСессиюToolStripMenuItem,
+            this.сохранитьСессиюToolStripMenuItem,
+            this.печатьToolStripMenuItem,
+            this.распечататьToolStripMenuItem,
+            this.открытьПоследнийЗакрытыйФайлToolStripMenuItem,
+            this.открытьВсеНедавниеФайлыToolStripMenuItem,
+            this.очиститьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // новыйToolStripMenuItem
+            // 
+            this.новыйToolStripMenuItem.Name = "новыйToolStripMenuItem";
+            this.новыйToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.новыйToolStripMenuItem.Text = "Новый";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть...";
+            // 
+            // открытьПапкуДокументаToolStripMenuItem
+            // 
+            this.открытьПапкуДокументаToolStripMenuItem.Name = "открытьПапкуДокументаToolStripMenuItem";
+            this.открытьПапкуДокументаToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.открытьПапкуДокументаToolStripMenuItem.Text = "Открыть папку документа";
+            // 
+            // открытьСтандартнымПриожениемToolStripMenuItem
+            // 
+            this.открытьСтандартнымПриожениемToolStripMenuItem.Name = "открытьСтандартнымПриожениемToolStripMenuItem";
+            this.открытьСтандартнымПриожениемToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.открытьСтандартнымПриожениемToolStripMenuItem.Text = "Открыть стандартным приожением";
+            // 
+            // выбратьПапкуКакПроектToolStripMenuItem
+            // 
+            this.выбратьПапкуКакПроектToolStripMenuItem.Name = "выбратьПапкуКакПроектToolStripMenuItem";
+            this.выбратьПапкуКакПроектToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.выбратьПапкуКакПроектToolStripMenuItem.Text = "Выбрать Папку как Проект";
+            // 
+            // обновитьСДискаToolStripMenuItem
+            // 
+            this.обновитьСДискаToolStripMenuItem.Name = "обновитьСДискаToolStripMenuItem";
+            this.обновитьСДискаToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.обновитьСДискаToolStripMenuItem.Text = "Обновить с диска";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как..";
+            // 
+            // сохранитьКопиюКакToolStripMenuItem
+            // 
+            this.сохранитьКопиюКакToolStripMenuItem.Name = "сохранитьКопиюКакToolStripMenuItem";
+            this.сохранитьКопиюКакToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.сохранитьКопиюКакToolStripMenuItem.Text = "Сохранить копию как...";
+            // 
+            // сохранитьВсеToolStripMenuItem
+            // 
+            this.сохранитьВсеToolStripMenuItem.Name = "сохранитьВсеToolStripMenuItem";
+            this.сохранитьВсеToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.сохранитьВсеToolStripMenuItem.Text = "Сохранить все";
+            // 
+            // переименоватьToolStripMenuItem
+            // 
+            this.переименоватьToolStripMenuItem.Name = "переименоватьToolStripMenuItem";
+            this.переименоватьToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.переименоватьToolStripMenuItem.Text = "Переименовать...";
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            // 
+            // закрытьВсеToolStripMenuItem
+            // 
+            this.закрытьВсеToolStripMenuItem.Name = "закрытьВсеToolStripMenuItem";
+            this.закрытьВсеToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.закрытьВсеToolStripMenuItem.Text = "Закрыть все";
+            // 
+            // управлениеВкладкамиToolStripMenuItem
+            // 
+            this.управлениеВкладкамиToolStripMenuItem.Name = "управлениеВкладкамиToolStripMenuItem";
+            this.управлениеВкладкамиToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.управлениеВкладкамиToolStripMenuItem.Text = "Управление вкладками...";
+            // 
+            // удалитьФайлвКорзинуToolStripMenuItem
+            // 
+            this.удалитьФайлвКорзинуToolStripMenuItem.Name = "удалитьФайлвКорзинуToolStripMenuItem";
+            this.удалитьФайлвКорзинуToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.удалитьФайлвКорзинуToolStripMenuItem.Text = "Удалить файл(в корзину)";
+            // 
+            // загрузитьСессиюToolStripMenuItem
+            // 
+            this.загрузитьСессиюToolStripMenuItem.Name = "загрузитьСессиюToolStripMenuItem";
+            this.загрузитьСессиюToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.загрузитьСессиюToolStripMenuItem.Text = "Загрузить сессию...";
+            // 
+            // сохранитьСессиюToolStripMenuItem
+            // 
+            this.сохранитьСессиюToolStripMenuItem.Name = "сохранитьСессиюToolStripMenuItem";
+            this.сохранитьСессиюToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.сохранитьСессиюToolStripMenuItem.Text = "Сохранить сессию...";
+            // 
+            // печатьToolStripMenuItem
+            // 
+            this.печатьToolStripMenuItem.Name = "печатьToolStripMenuItem";
+            this.печатьToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.печатьToolStripMenuItem.Text = "Печать...";
+            // 
+            // распечататьToolStripMenuItem
+            // 
+            this.распечататьToolStripMenuItem.Name = "распечататьToolStripMenuItem";
+            this.распечататьToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.распечататьToolStripMenuItem.Text = "Распечатать!";
+            // 
+            // открытьПоследнийЗакрытыйФайлToolStripMenuItem
+            // 
+            this.открытьПоследнийЗакрытыйФайлToolStripMenuItem.Name = "открытьПоследнийЗакрытыйФайлToolStripMenuItem";
+            this.открытьПоследнийЗакрытыйФайлToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.открытьПоследнийЗакрытыйФайлToolStripMenuItem.Text = "Открыть последний закрытый файл";
+            // 
+            // открытьВсеНедавниеФайлыToolStripMenuItem
+            // 
+            this.открытьВсеНедавниеФайлыToolStripMenuItem.Name = "открытьВсеНедавниеФайлыToolStripMenuItem";
+            this.открытьВсеНедавниеФайлыToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.открытьВсеНедавниеФайлыToolStripMenuItem.Text = "Открыть все недавние файлы";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.очиститьToolStripMenuItem.Text = "Очистить список недавних файлов";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новыйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьПапкуДокументаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьСтандартнымПриожениемToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выбратьПапкуКакПроектToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem обновитьСДискаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКопиюКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьВсеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem переименоватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьВсеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem управлениеВкладкамиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьФайлвКорзинуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьСессиюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьСессиюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem печатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem распечататьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьПоследнийЗакрытыйФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьВсеНедавниеФайлыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
     }
 }
 
